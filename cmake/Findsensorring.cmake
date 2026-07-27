f6b9052556d4f1623120de9139810aedc72181e8
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-07-27 15:39:36 +0200

    feat: update sensor ring library version

--- cmake/Findsensorring.cmake
+++ cmake/Findsensorring.cmake
@@ -44,7 +44,7 @@ if(NOT TARGET sensorring::sensorring)
   FetchContent_Declare(
     sensorring_source
     URL
-    https://github.com/EduArt-Robotik/edu_lib_sensorring/archive/refs/tags/v4.0.0-alpha.6.zip
+    https://github.com/EduArt-Robotik/edu_lib_sensorring/archive/refs/tags/v4.0.0-alpha.7.zip
   )
   FetchContent_MakeAvailable(sensorring_source)
   cmake_policy(POP)
